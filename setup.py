ef177606a899e819cf103d34296cce1cc0a22017
Sergey Mezentsev <thebits@yandex.ru>
2021-03-26 17:22:48 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,24 @@
 import setuptools
 
-with open('README.md', 'r') as fh:
+with open("README.md") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ruvdsdriver',
-    version='0.0.1',
-    author='Sergey Mezentsev',
-    author_email='thebits@yandex.ru',
-    description='Libcloud driver for RU VDS',
-    install_requires=['apache-libcloud>=3.0.0'],
-    license='UNLICENSE',
-    long_description_content_type='text/markdown',
+    name="ruvdsdriver",
+    version="0.0.1",
+    author="Sergey Mezentsev",
+    author_email="thebits@yandex.ru",
+    description="Libcloud driver for RU VDS",
+    install_requires=["apache-libcloud>=3.0.0"],
+    license="UNLICENSE",
+    long_description_content_type="text/markdown",
     long_description=long_description,
-    url='https://github.com/thebits/libcloud-ruvds',
+    url="https://github.com/thebits/libcloud-ruvds",
     packages=setuptools.find_packages(),
     classifiers=[
-        'Intended Audience :: System Administrators',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Intended Audience :: System Administrators",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
-
